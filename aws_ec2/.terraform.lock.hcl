39967ae670ac49e7815829df450feb0ef899784b
Ondrej Sika <ondrej@ondrejsika.com>
2022-08-22 22:16:43 +0200

    feat(aws_ec2): Add EC2 example (with Infracost)

---
+++ aws_ec2/.terraform.lock.hcl
@@ -0,0 +1,10 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "4.27.0"
+  constraints = "4.27.0"
+  hashes = [
+    "h1:OKB0tanWDnG2hYs2IHOwmFYpEb+YjSaUxtuRzgdDCw4=",
+  ]
+}
---
+++ aws_ec2/main.tf
@@ -0,0 +1,37 @@
+resource "aws_key_pair" "ondrejsika" {
+  key_name   = "ondrejsika"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCslNKgLyoOrGDerz9pA4a4Mc+EquVzX52AkJZz+ecFCYZ4XQjcg2BK1P9xYfWzzl33fHow6pV/C6QC3Fgjw7txUeH7iQ5FjRVIlxiltfYJH4RvvtXcjqjk8uVDhEcw7bINVKVIS856Qn9jPwnHIhJtRJe9emE7YsJRmNSOtggYk/MaV2Ayx+9mcYnA/9SBy45FPHjMlxntoOkKqBThWE7Tjym44UNf44G8fd+kmNYzGw9T5IKpH1E1wMR+32QJBobX6d7k39jJe8lgHdsUYMbeJOFPKgbWlnx9VbkZh+seMSjhroTgniHjUl8wBFgw0YnhJ/90MgJJL4BToxu9PVnH ondrej@ondrejsika.com"
+}
+
+resource "aws_security_group" "allow_all" {
+  name = "allow_all"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-049ed5fa529109ac4"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.ondrejsika.key_name
+  security_groups = [
+    aws_security_group.allow_all.name,
+  ]
+}
+
+output "ip" {
+  value = aws_instance.ec2.public_ip
+}
+
+output "ssh" {
+  value = "admin@${aws_instance.ec2.public_ip}"
+}
