68d92da9d5956c8be84b33eb8358712971d3e779
Ondrej Sika <ondrej@ondrejsika.com>
2023-12-11 23:30:52 +0100

    feat(azure_linux_machine_module): Use user default (instead of demo)

--- azure_linux_machine_module/main.tf
+++ azure_linux_machine_module/main.tf
@@ -2,7 +2,7 @@ locals {
   name           = "linux-example"
   location       = "westeurope"
   size           = "Standard_B1ls"
-  admin_username = "demo"
+  admin_username = "default"
   admin_password = "asdfasdf1234A."
   admin_ssh_key  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCslNKgLyoOrGDerz9pA4a4Mc+EquVzX52AkJZz+ecFCYZ4XQjcg2BK1P9xYfWzzl33fHow6pV/C6QC3Fgjw7txUeH7iQ5FjRVIlxiltfYJH4RvvtXcjqjk8uVDhEcw7bINVKVIS856Qn9jPwnHIhJtRJe9emE7YsJRmNSOtggYk/MaV2Ayx+9mcYnA/9SBy45FPHjMlxntoOkKqBThWE7Tjym44UNf44G8fd+kmNYzGw9T5IKpH1E1wMR+32QJBobX6d7k39jJe8lgHdsUYMbeJOFPKgbWlnx9VbkZh+seMSjhroTgniHjUl8wBFgw0YnhJ/90MgJJL4BToxu9PVnH"
   user_data = base64encode(
