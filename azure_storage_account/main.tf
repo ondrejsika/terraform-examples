d0876707a7c2f6984dc2e38ace64db95b9cf612d
Ondrej Sika <ondrej@ondrejsika.com>
2025-03-10 09:34:08 +0100

    feat(azure_storage_account): Use local zone replication

--- azure_storage_account/main.tf
+++ azure_storage_account/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_storage_account" "example" {
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "example" {
